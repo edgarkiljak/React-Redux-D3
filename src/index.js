cd3cdbe221dd48a0203b9af733a372f3c7271a36
Edgar Kiljak <edgar.kiljak@adylic.com>
2020-03-28 21:47:22 +0000

    removed unnecessary files and references

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import store from './Redux/store';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore'; //<- needed if using firestore
 import firebase from './config/firebase';
@@ -30,8 +29,3 @@ ReactDOM.render(
   </Provider>,
   target
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
